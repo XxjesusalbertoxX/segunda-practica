9065876be59d4f15845b3569792e565b4a1eddbe
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2025-06-06 11:40:23 -0600

    sdssds

--- clases/Grupo.py
+++ clases/Grupo.py
@@ -1,14 +1,9 @@
-try:
-    from .Lista import Lista
-    from .Alumno import Alumno
-    from .Maestro import Maestro
-except ImportError:
-    from Lista import Lista
-    from Alumno import Alumno
-    from Maestro import Maestro
+from .Lista import Lista
+from .Alumno import Alumno
+from .Maestro import Maestro
 
 class Grupo(Lista):
-    def __init__(self, Nombre=None, Maestro=None, Alumnos=None):
+    def __init__(self, Nombre = None, Maestro = None, Alumnos=None):
         if Nombre and Maestro and Alumnos:
             self.Nombre = Nombre
             self.Maestro = Maestro
@@ -20,7 +15,7 @@ class Grupo(Lista):
 
     def __str__(self):
         if hasattr(self, 'Nombre'):
-            return f"Grupo: {self.Nombre}, Maestro: {self.Maestro}, Alumnos: {self.Alumnos}"
+            return f"(Grupo: {self.Nombre}), (Maestro: {self.Maestro}), (Alumnos: {self.Alumnos})"
         else:
             return "Numero de Grupos " + str(len(self.lista))
 
@@ -73,11 +68,9 @@ class Grupo(Lista):
 if __name__ == "__main__":
     """
     maestro1 = Maestro(1, "Carlos", "Sánchez", 35, "Matemáticas")
-    maestro2 = Maestro(1, "Juanito", "sola", 35, "Español")
-    maestro3 = Maestro(1, "Carlos", "Pedro", 35, "Ingles")
-    a1 = Alumno(1, "Juan", "Pérez", 20, "123456")
-    a2 = Alumno(2, "Ana", "Gómez", 22, "654321")
-    a3 = Alumno(3, "Luis", "Martínez", 21, "789012")
+    alumno1 = Alumno(1, "Juan", "Pérez", 20, "123456")
+    alumno2 = Alumno(2, "Ana", "Gómez", 22, "654321")
+    alumno3 = Alumno(3, "Luis", "Martínez", 21, "789012")
 
     alumnos = Alumno()
     alumnos.agregar(alumno1)
